034702cda1c22f721ac990695118a7c370f9520f
Poonam-dew <dewanganpoonam778@gmail.com>
2024-12-01 22:05:02 +0530

    changes major .....

--- src/Pages/EventPage.js
+++ src/Pages/EventPage.js
@@ -1,25 +1,26 @@
 import React, { useState } from 'react';
 import '../Styles/EventPage.css';
 import radioLogo from '../Assets/radioLogo.png';
+import rtpB from '../Assets/rtpB.png';
 
 const events = [
     {
         date: "October 6, 2023",
         title: "Navigate Your Career Path",
-        description: "Description of Navigate Your Career Path event...",
-        image: {radioLogo}
+        description: "Description of Navigate Your Career Path event... Started in 2022, the RTP(Radio Telescope Project) at the Indian Institute of Technology in collaboration with PaAC(Physics and Astronomy Club) focuses on providing students with practical experience in radio astronomy. With a 5-meter aperture radio telescope, the project enables students to learn how to detect and analyze radio signals from space.The system has been tested with an Affordable Small Radio Telescope (ASRT) to ensure it meets the necessary standards for educational use. The main goal of the project is to give students hands-on training in astronomy while encouraging curiosity about astrophysics and space exploration.",
+        image: radioLogo
     },
     {
         date: "October 1, 2023",
         title: "Swachhta hi Sewa campaign",
         description: "IIT Roorkee with the help of team UBA in association with NSS and NCC organised awareness campaigns in its adopted Villages Meerpur and Puranpur on 1st October, 2023.",
-        image: "path/to/swachhta-campaign-image.jpg"
+        image: rtpB
     },
     {
         date: "August 24, 2023 - September 02, 2023",
         title: "Sociothon",
         description: "Results: Winner - SocioMonks",
-        image: "path/to/sociothon-image.jpg"
+        image: rtpB
     }
 ];
 
